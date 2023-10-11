4049c32e33e462329f0bf117ec1038c3d3e6731e
Pauldesch <100124809+Pauldesch@users.noreply.github.com>
2023-10-11 15:21:03 +0200

    Update SSquiz.js

--- SSquiz.js
+++ SSquiz.js
@@ -371,7 +371,7 @@ $('#final-button').on('click', function() {
     // Display animation container
     let loaderContainer = document.getElementById("final-progress");
     loaderContainer.style.display = 'block';
-    arrowElement.style.display = "none"; // Hide the arrow
+    
 
     // Play animation
     const player = document.querySelector("lottie-player");
